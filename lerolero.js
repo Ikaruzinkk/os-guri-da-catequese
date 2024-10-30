60417fe0e9bc90605da79b8f3656182f9018cf5c
Ikaruzinkk <patoikarus7@gmail.com>
2024-10-30 13:37:41 -0400

    first commit

---
+++ lerolero.js
@@ -0,0 +1,16 @@
+const txt = document.querySelector('#txt');
+const divImg = document.querySelector('.divImg');
+
+function bordaDiv() {
+    divImg.style.border='2px solid black'
+}
+
+divImg.addEventListener()
+
+txt.onclick = () => {
+    alert('Campo de texto clicado')
+}
+
+const clicked = () => {
+    console.log('Botão Clicado!')
+}
