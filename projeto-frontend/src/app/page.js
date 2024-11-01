5aaec3d6e591f33d05b7e7737d880d60a10e0528
Ikaruzinkk <patoikarus7@gmail.com>
2024-11-01 14:22:53 -0400

    wiat they dont love uou like i love

---
+++ projeto-frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>meu titulo</h1>
+      <input type="text" />
+    </div>
+  );
+}
